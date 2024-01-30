06a1dd2e40fa7a0b29905f9277095f7df5585dfc
Cszart <bitemecesar@gmail.com>
2024-01-30 14:29:58 -0400

    Fix: minor user page fix

--- pages/settings/user.tsx
+++ pages/settings/user.tsx
@@ -1,24 +1,30 @@
+import withAuthorizationServerSide from 'components/auth/withAuthorizationServerSide';
 import UsersScreen from 'components/screens/settings/user';
+import RolesEnum from 'const/role';
+import AppRoutes from 'const/routes';
+import { GetServerSideProps, Redirect } from 'next';
+import { getSession } from 'next-auth/react';
+import { crudPermissions } from 'utils';
 
 const UsersPage = (): JSX.Element => {
 	return <UsersScreen />;
 };
 
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-// 	const session = await getSession(context);
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const session = await getSession(context);
 
-// 	const redirect: Redirect | undefined = await withAuthorizationServerSide({
-// 		session: session,
-// 		allowedUsers: crudUsers(),
-// 		allowedUsers: [UsersEnum.ADMIN],
-// 		redirectTo: AppRoutes.HOME,
-// 	});
+	const redirect: Redirect | undefined = await withAuthorizationServerSide({
+		session: session,
+		allowedPermissions: crudPermissions(),
+		allowedRoles: [RolesEnum.ADMIN],
+		redirectTo: AppRoutes.HOME,
+	});
 
-// 	return {
-// 		props: {},
-// 		redirect: redirect,
-// 	};
-// };
+	return {
+		props: {},
+		redirect: redirect,
+	};
+};
 
 // export default withAuthorization(
 // 	UsersScreen,
