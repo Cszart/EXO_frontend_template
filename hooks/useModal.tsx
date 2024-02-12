6253838e51bbd0560cb2475eabe4accf61321259
Cszart <bitemecesar@gmail.com>
2024-02-12 19:46:01 -0400

    Fix: useCallback on modal

--- hooks/useModal.tsx
+++ hooks/useModal.tsx
@@ -1,6 +1,6 @@
 import { Transition, Dialog } from '@headlessui/react';
 import clsx from 'clsx';
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useCallback, useState } from 'react';
 
 interface ModalProps {
 	title?: string;
@@ -36,61 +36,66 @@ const useModal = (): {
 		setIsOpen(false);
 	};
 
-	const Modal = (props: ModalProps): JSX.Element => (
-		<Transition appear show={isOpen} as={Fragment}>
-			<Dialog
-				as="div"
-				className="relative z-10"
-				onClose={hide}
-				static
-				initialFocus={cancelButtonRef}
-			>
-				<Transition.Child
-					as={Fragment}
-					enter="ease-out duration-300"
-					enterFrom="opacity-0"
-					enterTo="opacity-100"
-					leave="ease-in duration-200"
-					leaveFrom="opacity-100"
-					leaveTo="opacity-0"
+	const Modal: React.FC<ModalProps> = useCallback(
+		(props) => (
+			<Transition.Root appear show={isOpen} as={Fragment}>
+				<Dialog
+					as="div"
+					className="relative z-10"
+					onClose={hide}
+					static
+					initialFocus={cancelButtonRef}
 				>
+					<Transition.Child
+						as={Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0"
+					>
+						<div
+							className="fixed inset-0"
+							style={{ background: 'rgba(33, 37, 41, 0.4)' }}
+						/>
+					</Transition.Child>
+
 					<div
-						className="fixed inset-0"
-						style={{ background: 'rgba(33, 37, 41, 0.4)' }}
-					/>
-				</Transition.Child>
-
-				<div className={clsx('fixed inset-0 overflow-y-auto', props.className)}>
-					<div className="flex h-full items-center  justify-center p-4 text-center">
-						<Transition.Child
-							as={Fragment}
-							enter="ease-out duration-300"
-							enterFrom="opacity-0 scale-95"
-							enterTo="opacity-100 scale-100"
-							leave="ease-in duration-200"
-							leaveFrom="opacity-100 scale-100"
-							leaveTo="opacity-0 scale-95"
-						>
-							<Dialog.Panel
-								className={clsx(
-									'transform shadow-xl transition-all',
-									'rounded-2xl bg-white w-full max-w-md p-6 text-left align-middle'
-								)}
+						className={clsx('fixed inset-0 overflow-y-auto', props.className)}
+					>
+						<div className="flex h-full items-center  justify-center p-4 text-center">
+							<Transition.Child
+								as={Fragment}
+								enter="ease-out duration-300"
+								enterFrom="opacity-0 scale-95"
+								enterTo="opacity-100 scale-100"
+								leave="ease-in duration-200"
+								leaveFrom="opacity-100 scale-100"
+								leaveTo="opacity-0 scale-95"
 							>
-								<Dialog.Title
-									as="h3"
-									className="text-lg font-bold text-gray-900"
+								<Dialog.Panel
+									className={clsx(
+										'transform shadow-xl transition-all',
+										'rounded-2xl bg-white w-full max-w-md p-6 text-left align-middle'
+									)}
 								>
-									{props.title}
-								</Dialog.Title>
+									<Dialog.Title
+										as="h3"
+										className="text-lg font-bold text-gray-900"
+									>
+										{props.title}
+									</Dialog.Title>
 
-								{props.children}
-							</Dialog.Panel>
-						</Transition.Child>
+									{props.children}
+								</Dialog.Panel>
+							</Transition.Child>
+						</div>
 					</div>
-				</div>
-			</Dialog>
-		</Transition>
+				</Dialog>
+			</Transition.Root>
+		),
+		[isOpen]
 	);
 
 	return { Modal, isOpen, show, hide };
