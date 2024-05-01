72766951f1026b1cd29d27b0a0bfea0ede24386e
Cszart <bitemecesar@gmail.com>
2024-05-01 11:36:22 -0400

    Fix: Build errors

--- pages/email/contentEditor.tsx
+++ pages/email/contentEditor.tsx
@@ -1,20 +1,11 @@
 import withAuthorizationServerSide from 'components/auth/withAuthorizationServerSide';
 import EmailContentEditorScreen from 'components/screens/email/contentEditor';
-import EmailContentEditorAceScreen from 'components/screens/email/contentEditorAce';
 import RolesEnum from 'const/role';
 import AppRoutes from 'const/routes';
 import { GetServerSideProps, Redirect } from 'next';
 import { getSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
 
 const EmailContentEditor = (): JSX.Element => {
-	const router = useRouter();
-	const { ace } = router.query;
-
-	if (ace != null && ace != undefined && ace == 'true') {
-		return <EmailContentEditorAceScreen />;
-	}
-
 	return <EmailContentEditorScreen />;
 };
 
